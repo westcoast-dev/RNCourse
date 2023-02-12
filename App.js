f3ec4832dcb4b10a86544044c4775ebadc1c62fc
westcoat-dev <westcoast@swave.kr>
2023-02-12 23:27:17 +0900

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder="목표를 입력하세요" />
+        <Button title="목표 추가" />
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
+      {/* <Text style={styles.dummyText}>This is WestCoast!!!!</Text>
+      <Text style={styles.dummyText}>Test App</Text>
+      <Button
+        title="로그인"
+        onPress={() => {
+          Alert.alert("로그인 성공");
+        }}
+      /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50,
+    paddingTop: 100,
   },
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: "#fff",
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  // },
+  // dummyText: {
+  //   margin: 16,
+  //   borderWidth: 2,
+  //   borderColor: "#87faca",
+  //   padding: 16,
+  // },
 });
